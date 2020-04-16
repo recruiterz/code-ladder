4b33fa3b787e1ba9026433ce86e7a6ed854aa309
sol - newinh <newinh90@gmail.com>
2020-04-17 01:19:41 +0900

    Update newinh-longest-common-prefix.py

--- 2020-04-16-longest-commen-prefix/newinh-longest-common-prefix.py
+++ 2020-04-16-longest-commen-prefix/newinh-longest-common-prefix.py
@@ -16,4 +16,56 @@ class Solution:
                     return min_str[:i]
         
         return min_str
+    
+###############################################################################
+## solve with trie
+##########
+
+class TrieNode:
+
+    def __init__(self, key=None):
+        self.children = dict()
+
+# Your Trie object will be instantiated and called as such:
+# obj = Trie()
+# obj.insert(word)
+# param_2 = obj.search(word)
+# param_3 = obj.startsWith(prefix)
+
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        
+        if not strs:
+            return ''
+        
+        root = TrieNode()
+        first_word = min(strs, key=len)
+        
+        node = root
+        for s in first_word:
+            node.children[s] = TrieNode()
+            node = node.children[s]
+            
+        prefix = ''
+        for word in strs:
+            
+            node = root
+            for i, s in enumerate(word):
+                
+                if s in node.children:
+                    node = node.children[s]
+                else:
+                    node.children = {}
+                    break
+        
+        if not root.children:
+            return ''
+        
+        prefix = ''
+        node = root
+        while node.children:
+            prefix += list(node.children.keys())[0]
+            node = list(node.children.values())[0]
+            
+        return prefix
 
