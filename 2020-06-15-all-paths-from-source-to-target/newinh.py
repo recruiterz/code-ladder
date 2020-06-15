846044f1b80ab04725b19c10412e8fdddf67ab11
sol <newinh90@gmail.com>
2020-06-15 22:43:10 +0900

    Create newinh.py

---
+++ 2020-06-15-all-paths-from-source-to-target/newinh.py
@@ -0,0 +1,33 @@
+class Solution:
+    def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
+        
+        stack = []
+        paths = []
+        
+        stack.append((set(), 0))
+        
+        while stack:
+            
+            path, v = stack.pop()
+            path = path.copy()
+            path.add(v)
+            
+            if not graph[v]:
+                paths.append(path)
+                continue
+
+            to_go = []
+            for _v in graph[v]:
+                if _v in path:
+                    paths.append(path)
+                else:
+                    to_go.append(_v)
+                    
+            for _to_go in to_go:
+                stack.append((path, _to_go))
+        
+        result = []
+        for path in paths:
+            result.append(sorted(list(path)))
+            
+        return result
