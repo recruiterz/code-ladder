14a02ac5066a526261208f9abb851d32040a3738
sol <newinh90@gmail.com>
2020-07-15 22:48:58 +0900

    Update newinh.py

--- 2020-07-15-island-perimeter/newinh.py
+++ 2020-07-15-island-perimeter/newinh.py
@@ -1,3 +1,23 @@
 class Solution:
-    def reverseWords(self, s: str) -> str:
-        return ' '.join(reversed(s.split()))
+    def islandPerimeter(self, grid: List[List[int]]) -> int:
+        
+        height = len(grid)
+        width = len(grid[0])
+    
+        def is_island(h, w) -> bool:
+            return grid[h][w] == 1
+        
+        
+        def count_border(h, w) -> int:
+            nn = int(h == 0 or grid[h-1][w] == 0)
+            ss = int(h == height -1 or grid[h+1][w] == 0)
+            ww = int(w == 0 or grid[h][w-1] == 0)
+            ee = int(w == width - 1 or grid[h][w+1] == 0)
+            return nn + ss + ww + ee
+        
+        perimeter = 0
+        for _h in range(height):
+            for _w in range(width):
+                if is_island(_h, _w):
+                    perimeter += count_border(_h, _w)
+        return perimeter
