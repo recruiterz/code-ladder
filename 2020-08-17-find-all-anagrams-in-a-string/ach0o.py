0d7eda953f668643e78eb14ccbcddc0d7d18546c
ach0o <88soldieron@gmail.com>
2020-08-18 00:10:48 +0900

    Update ach0o solution

--- 2020-08-17-find-all-anagrams-in-a-string/ach0o.py
+++ 2020-08-17-find-all-anagrams-in-a-string/ach0o.py
@@ -1,17 +1,27 @@
+from collections import Counter
+
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
         ans = []
-        char = []
-        _p = sorted(p)
+
+        p_cnts = Counter(p)
+        p_size = len(p)
+
+        ana_candidate = s[:len(p)]
+        cand_cnts = Counter(ana_candidate)
+
         index = 0
+        for c in s[p_size:]:
+            if p_cnts == cand_cnts:
+                ans.append(index)
 
-        for c in s:
-            char.append(c)
+            if cand_cnts[s[index]] == 1:
+                del cand_cnts[s[index]]
+            else:
+                cand_cnts[s[index]] -= 1
+            cand_cnts[c] += 1
+            index += 1
 
-            if len(char) == len(p):
-                if sorted(char) == _p:
-                    ans.append(index)
-                char.pop(0)
-                index += 1
-
+        if p_cnts == cand_cnts:
+            ans.append(index)
         return ans
