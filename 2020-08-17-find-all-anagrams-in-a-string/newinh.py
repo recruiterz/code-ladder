e8655072211c0be565d714e7980ed6038875a432
sol <newinh90@gmail.com>
2020-08-17 22:31:51 +0900

    Update newinh.py
    little improve

--- 2020-08-17-find-all-anagrams-in-a-string/newinh.py
+++ 2020-08-17-find-all-anagrams-in-a-string/newinh.py
@@ -3,24 +3,34 @@ from collections import Counter
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
         
-        p_counter = Counter(p)
-        
-        def is_anagram(slice_s) -> bool:
-            c = Counter(slice_s)
-            
-            if len(p_counter) != len(c):
+        def is_anagram(c1, c2) -> bool:
+            if len(c1) != len(c2):
                 return False
             
-            for k in c.keys():
-                if c[k] != p_counter.get(k, 0):
+            for k in c1.keys():
+                if c1[k] != c2.get(k, 0):
                     return False
             return True
+            
         
         len_p = len(p)
         len_s = len(s)
         
+        p_counter = Counter(p)
+        s_counter = Counter(s[:len_p])
+        
         output = []
         for i in range(0, len_s - len_p + 1):
-            if is_anagram(s[i:i+len_p]):
+            if i != 0:
+                sliced_s_to_delete = s[i-1]
+                s_counter[sliced_s_to_delete] -= 1
+                if s_counter[sliced_s_to_delete] == 0:
+                    s_counter.pop(sliced_s_to_delete)
+                
+                sliced_s_to_add = s[i + len_p - 1]
+                count = s_counter.setdefault(sliced_s_to_add, 0)
+                s_counter[sliced_s_to_add] = count + 1
+                
+            if is_anagram(p_counter, s_counter):
                 output.append(i)
         return output
