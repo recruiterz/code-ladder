5a8a2d18efad22a274679a571db6fb1f5204830b
sol <newinh90@gmail.com>
2020-10-05 22:25:22 +0900

    Create newinh.py
    count and replace

---
+++ 2020-10-05-sort-colors/newinh.py
@@ -0,0 +1,27 @@
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        
+        n_0 = 0
+        n_1 = 0
+        n_2 = 0
+        
+        for i in nums:
+            if i == 0:
+                n_0 += 1
+            elif i == 1:
+                n_1 += 1
+            elif i == 2:
+                n_2 += 1
+                
+        for i in range(len(nums)):
+            
+            if i < n_0:
+                nums[i] = 0
+            elif n_0 <= i < n_0 + n_1:
+                nums[i] = 1
+            else:
+                nums[i] = 2
+
