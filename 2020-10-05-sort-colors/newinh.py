2c1a53caeaa8961f98f70012600bf34cda067d9a
sol <newinh90@gmail.com>
2020-10-05 22:27:32 +0900

    Update newinh.py

--- 2020-10-05-sort-colors/newinh.py
+++ 2020-10-05-sort-colors/newinh.py
@@ -3,18 +3,15 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        
+
         n_0 = 0
         n_1 = 0
-        n_2 = 0
         
         for i in nums:
             if i == 0:
                 n_0 += 1
             elif i == 1:
                 n_1 += 1
-            elif i == 2:
-                n_2 += 1
                 
         for i in range(len(nums)):
             
@@ -24,4 +21,3 @@ class Solution:
                 nums[i] = 1
             else:
                 nums[i] = 2
-
